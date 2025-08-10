51fb687d7e80bbc14f55aebf8ad57fd022fbb223
lenerystia <marlenamakosza@gmail.com>
2025-08-10 21:57:37 +0200

    feat: update scheema about number to age and checking is age above 0

--- src/lib/server/db/schema/schema.ts
+++ src/lib/server/db/schema/schema.ts
@@ -1,11 +1,16 @@
-import { numeric, pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import { bigint, check, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
-export const characters = pgTable('characters', {
-	id: serial('id').primaryKey(),
-	firstName: text('first_name').notNull(),
-	lastName: text('last_name').notNull(),
-	age: numeric('age').notNull(),
-});
+export const characters = pgTable(
+	'characters',
+	{
+		id: serial('id').primaryKey(),
+		firstName: text('first_name').notNull(),
+		lastName: text('last_name').notNull(),
+		age: bigint('age', { mode: 'number' }).notNull(),
+	},
+	table => [check('age_non_negative', sql`${table.age} >= 0`)],
+);
 
 export const relations = pgTable('relations', {
 	id: serial('id').primaryKey(),
