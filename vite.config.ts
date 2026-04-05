1a0005c65125a81ece65b8d3abf4231fdde21f7d
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-04-05 11:35:12 +0200

    configure vite and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,15 @@
-import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'@src': path.resolve(__dirname, 'src'),
-			'@routes': path.resolve(__dirname, 'src/routes'),
-			'@test': path.resolve(__dirname, 'test/unit'),
-			'@testRoot': path.resolve(__dirname, 'test/e2e'),
-		},
-	},
-	test: {
-		workspace: [
-			{
-				extends: './vite.config.ts',
-				plugins: [svelteTesting()],
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					//TODO - move some there
-					//		exclude: [...configDefaults.exclude, 'packages/template/*'],
-					// 		include: ['./tests/unit/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@test': path.resolve(__dirname, 'test/unit'),
+      '@testRoot': path.resolve(__dirname, 'test/e2e'),
+    },
+  },
 });
