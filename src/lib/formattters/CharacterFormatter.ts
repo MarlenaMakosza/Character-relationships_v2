69c91f06d5bab6b587623c1fc8a67bccc49e8f8b
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-04-05 13:59:51 +0200

    chore: restore formatter files as commented-out placeholders

---
+++ src/lib/formattters/CharacterFormatter.ts
@@ -0,0 +1,47 @@
+// import type { Character } from '$lib/domain/Character';
+//
+//
+// export class CharacterFormatter {
+//
+//   public static toPOJO(character: Character): {
+//     id: number;
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//   } {
+//     return {
+//       // V2 - doesn't work correct
+//       // character: structuredClone(character),
+//       // V1
+//       id: character.id,
+//       firstName: character.firstName,
+//       lastName: character.lastName,
+//       age: character.age,
+//     };
+//   }
+//
+//
+//   public static toPOJOs(characters: readonly Character[]): Array<{
+//     id: number;
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//   }> {
+//     return characters.map(character => this.toPOJO(character));
+//   }
+//
+//
+//   // public static getFullName(character: Character): string {
+//   //   return `${character.firstName} ${character.lastName}`;
+//   // }
+//
+//   // public static POJOfullName(character: Character) {
+//   //   return {
+//   //     id: character.id,
+//   //     firstName: character.firstName,
+//   //     lastName: character.lastName,
+//   //     fullName: this.getFullName(character)
+//   //   };
+//   // }
+//
+// }
