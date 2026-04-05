2e7add6b8101e404f513b956b1f719c475d3d907
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-04-05 13:25:45 +0200

    comment more doesn't working code

--- src/lib/validators/CharacterValidator.ts
+++ src/lib/validators/CharacterValidator.ts
@@ -1,14 +1,14 @@
-import type { Character } from '$lib/domain/Character';
+// import type { Character } from '$lib/domain/Character';
 
-const missingName = 0;
+// const missingName = 0;
 
-export const CharacterValidator = {
-  isValid(character: Character): boolean {
-    return (
-      character.names.length > missingName ||
-      character.surnames.length > missingName ||
-      character.description?.trim().length > missingName ||
-      character.labels.length > missingName
-    );
-  },
-};
+// export const CharacterValidator = {
+//   isValid(character: Character): boolean {
+//     return (
+//       character.names.length > missingName ||
+//       character.surnames.length > missingName ||
+//       character.description?.trim().length > missingName ||
+//       character.labels.length > missingName
+//     );
+//   },
+// };
