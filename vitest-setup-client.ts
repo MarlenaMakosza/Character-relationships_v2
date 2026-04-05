193ab18b6f45c2702fea54939ed434e285b149ac
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-04-05 11:24:48 +0200

    fix: fix lint errors in vitest client setup

--- vitest-setup-client.ts
+++ vitest-setup-client.ts
@@ -2,17 +2,17 @@ import '@testing-library/jest-dom/vitest';
 import { vi } from 'vitest';
 
 // required for svelte5 + jsdom as jsdom does not support matchMedia
-Object.defineProperty(window, 'matchMedia', {
+Object.defineProperty(globalThis, 'matchMedia', {
 	writable: true,
 	enumerable: true,
-	value: vi.fn().mockImplementation((query) => ({
+	value: vi.fn().mockImplementation((query: string) => ({
 		matches: false,
 		media: query,
 		onchange: null,
 		addEventListener: vi.fn(),
 		removeEventListener: vi.fn(),
-		dispatchEvent: vi.fn()
-	}))
+		dispatchEvent: vi.fn(),
+	})),
 });
 
 // add more mocks here if you need them
