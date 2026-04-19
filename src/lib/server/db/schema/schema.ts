859da93dd51a0b2beb82aa072dadf7f8c73f61b8
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-04-19 10:22:40 +0200

    fix: add defaultRandom() to character and relation id columns

--- src/lib/server/db/schema/schema.ts
+++ src/lib/server/db/schema/schema.ts
@@ -1,12 +1,12 @@
-import { pgTable, serial, text, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
 
 export const characters = pgTable('characters', {
-  id: uuid('id').primaryKey(),
+  id: uuid('id').primaryKey().defaultRandom(),
   name: text('name'),
 });
 
 export const relations = pgTable('relations', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey().defaultRandom(),
   idChar1: uuid('id_char_1').references(() => characters.id),
   idChar2: uuid('id_char_2').references(() => characters.id),
   about: text('about').notNull(),
