ac0abcd31a12ef4867b2a8b473fa71e75511990d
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-04-23 17:21:15 +0200

    chore(seeding): update seeding command in package.json

--- dev/db/migrate.ts
+++ dev/db/migrate.ts
@@ -10,7 +10,7 @@ if (!("DATABASE_URL" in env)) throw new Error("DATABASE_URL not found in .env!")
 
 const CONNECTION_LIMIT = 1;
 const connection = postgres(env.DATABASE_URL, { max: CONNECTION_LIMIT });
-// TODO: co jeśli connectiona nie ma
+// TODO: what if connection doesn't exist
 const db = drizzle(connection);
 
 await migrate(db, { migrationsFolder: "./dev/drizzle" });
